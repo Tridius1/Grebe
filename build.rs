c1ce42a54727a0eae03e46afcacd562af3ceba3a
TSwanson17 <tristan.t73@gmail.com>
2026-07-12 00:17:27 -0600

    Icon added to executable

---
+++ build.rs
@@ -0,0 +1,18 @@
+
+use winres;
+
+fn main() {
+    // Package exe with icon
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winres::WindowsResource::new();
+        
+        res.set_icon("grebe_icon.ico")
+        .set("OriginalFilename", "Grebe.exe")
+        .set("ProductName", "Grebe");
+        
+        if let Err(e) = res.compile() {
+            eprintln!("Failed to compile resources: {}", e);
+            std::process::exit(1);
+        }
+    }
+}
